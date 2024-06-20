8c50f25a8b78b666541d5d363f9f6d88feee3744
Rana Essam <76626570+Ranaaessam@users.noreply.github.com>
2024-06-20 16:49:26 +0300

    Cart modfied

--- screens/CheckList.js
+++ screens/CheckList.js
@@ -243,10 +243,10 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginBottom: 10,
     shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 2,
+    // },
     shadowOpacity: 0.25,
     shadowRadius: 2,
   },
