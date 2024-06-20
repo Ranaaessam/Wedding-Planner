3bc3b6ccb6c9a5b3e144f0c63d4589530e7026be
Khalid Elsayed <khalid972010@gmail.com>
2024-06-20 19:39:41 +0300

    trial

--- node/routes/users.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const UsersControllersb= require("../controllers/usersControllers");
-
-router.get("/",UsersControllersb.getAllUsers);
-
-module.exports = router;
