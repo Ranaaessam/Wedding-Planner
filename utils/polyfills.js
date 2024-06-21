86584883fa4c5056ecc7a0b32c7002c09716cef9
Rana Essam <76626570+Ranaaessam@users.noreply.github.com>
2024-06-21 14:25:40 +0300

    modfied paths

--- utils/polyfills.js
+++
@@ -1,5 +0,0 @@
-// // src/utils/polyfills.js
-// import 'react-native';
-// import '@formatjs/intl-pluralrules/polyfill';
-// import '@formatjs/intl-pluralrules/locale-data/en'; // Add locale data for languages you support
-// import '@formatjs/intl-pluralrules/locale-data/ar';
