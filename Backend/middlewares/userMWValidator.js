3309b507df0e668b9d560b1ce2c5a8242bb69a08
mahmouddhamdy <68087837+mahmouddhamdy@users.noreply.github.com>
2024-06-22 23:21:44 +0300

    Fix userController bug

--- Backend/middlewares/userMWValidator.js
+++ Backend/middlewares/userMWValidator.js
@@ -1,11 +1,9 @@
-const  validator = require("../Util/userValidator");
-module.exports=(req,res,next)=>{
-   let valid = validator(req.body);
-   if(valid)
-    {
-        req.valid=1;
-        next();
-    }
-    else
-    res.status(403).send("forbidden command");
-}
+const validator = require("../Util/userValidator");
+module.exports = (req, res, next) => {
+  let valid = validator(req.body);
+  console.log(valid);
+  if (valid) {
+    req.valid = 1;
+    next();
+  } else res.status(403).send("forbidden command");
+};
