6c1130a3647541293ab1be7b2aa63703442f2975
shima_atef <shimaatif731@gmail.com>
2024-06-22 17:46:25 +0300

    Suppliers Controller

--- Backend/routes/suppliers.js
+++ Backend/routes/suppliers.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const SuppliersController = require("../controllers/suppliersController");
 
 router.get("/", SuppliersController.getAllSuppliers);
+router.get("/filtersuppliersByType", SuppliersController.filtersuppliersByType);
+router.get("/getSuppliersByID", SuppliersController.getSuppliersByID);
+
 
 module.exports = router;
